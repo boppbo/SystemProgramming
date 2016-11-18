d3035f551730bb347cf0ad66e798d62c0c4e080c
Boris Bopp <boris.bopp@gmail.com>
2016-11-18 18:56:23 +0100

    Buffer: Add test code.

--- Buffer/src/TestBuffer.cpp
+++ Buffer/src/TestBuffer.cpp
@@ -1,10 +1,30 @@
 #include "../includes/Buffer.h"
+#include <iostream>
+using namespace std;
 
+void println(char const &ch) {
+	cout << (int)ch << "\t : " << ch << endl;
+}
+void printToEnd(Buffer& buffer) {
+	char c;
+	while(( c = buffer.getChar() )!= '\0') {
+			println(c);
+		}
+}
 
 int main(int argc, char **argv) {
+	if (argc != 2)
+		return EXIT_FAILURE;
 
-	Buffer*  buffer;
+	Buffer buffer(argv[1], 4);
 
-	buffer = new Buffer();
+	printToEnd(buffer);
+	cout << "---------------------------------" << endl;
+	for(int i = 0; i < 6; i++) {
+		println(buffer.ungetChar());
+	}
+	cout << "---------------------------------" << endl;
+	printToEnd(buffer);
 
+	return EXIT_SUCCESS;
 }
