1fc5bd88da4dcff203bb1aab47deac76870ee7d0
Boris Bopp <boris.bopp@gmail.com>
2016-12-04 08:29:57 +0100

    Automat: Add first implementation * Clean up Makefile * Copy automat from other repo

--- Automat/src/Automat.cpp
+++ Automat/src/Automat.cpp
@@ -4,7 +4,7 @@
  */
 
 #include "../includes/Automat.h"
-
+#include <ctype.h>
 
 Automat::Automat() {
 	// TODO Auto-generated constructor stub
@@ -14,3 +14,48 @@ Automat::Automat() {
 Automat::~Automat() {
 	// TODO Auto-generated destructor stub
 }
+
+action Automat::stateEval(event e) {
+
+	stateElement stateEvaluation = this->_stateMatrix[ this->_currentState ][ e ];
+
+	this->_currentState = stateEvaluation.nextState;
+
+	return stateEvaluation.actionToDo;
+}
+
+action Automat::charEval(char c) {
+
+	if (isdigit(c)) {
+		return this->stateEval(DIGIT);
+	}
+	if(isalpha(c)) {
+		return this->stateEval(LETTER);
+	}
+	if (isSimpleSign(c)) {
+		return this->stateEval(SIGN);
+	}
+	switch (c) {
+		case ':': return this->stateEval(COLON);
+		case '*': return this->stateEval(STAR);
+		case '=': return this->stateEval(EQUALS);
+		case '&': return this->stateEval(AND);
+		case ' ': return this->stateEval(SPACE);
+		case '\n': return this->stateEval(NEW_LINE);
+	}
+	if (c == '\0') {
+		return this->stateEval(EOF);
+	}
+
+	return this->stateEval(UNKNOWN);
+}
+
+
+bool Automat::isSimpleSign(char c) {
+
+	for (unsigned int i = 0; i < sizeof(signArray); i++) {
+		if (c == signArray[i])
+			return true;
+	}
+	return false;
+};
--- Automat/src/TestAutomat.cpp
+++ Automat/src/TestAutomat.cpp
@@ -1,9 +1,69 @@
 #include "../includes/Automat.h"
+#include <iostream>
+using namespace std;
+
+Automat* automat;
+
+void nothing() {
+	cout << "Action: NOTHING | nextState: " << automat->_currentState << endl;
+}
+void add_char() {
+	cout << "Action: ADD_CHAR | nextState: " << automat->_currentState << endl;
+}
+void add_token_integer() {
+	cout << "Action: ADD_TOKEN_INTEGER | nextState: " << automat->_currentState << endl;
+}
+void add_token_identifier() {
+	cout << "Action: ADD_TOKEN_IDENTIFIER | nextState: " << automat->_currentState << endl;
+}
+void add_token_sign() {
+	cout << "Action: ADD_TOKEN_SIGN | nextState: " << automat->_currentState << endl;
+}
+void add_token_sign_ug0() {
+	cout << "Action: ADD_TOKEN_SIGN_UG0 | nextState: " << automat->_currentState << endl;
+}
+void add_token_sign_ug2() {
+	cout << "Action: ADD_TOKEN_SIGN_UG2 | nextState: " << automat->_currentState << endl;
+}
+void add_token_unknown() {
+	cout << "Action: ADD_TOKEN_UKNOWN | nextState: " << automat->_currentState << endl;
+}
+void discard() {
+	cout << "Action: DISCARD | nextState: " << automat->_currentState << endl;
+}
+void end() {
+	cout << "Action: END | nextState: " << automat->_currentState << endl;
+}
+
 
 int main (int argc, char* argv[]){
 
-	Automat* automat;
-
 	automat = new Automat();
 
+	void (*tansition_action[10])() = {
+		nothing,
+		add_char,
+		add_token_integer,
+		add_token_identifier,
+		add_token_sign,
+		add_token_sign_ug0,
+		add_token_sign_ug2,
+		add_token_unknown,
+		discard,
+		end
+	};
+
+
+	tansition_action[automat->stateEval(DIGIT)]();
+	tansition_action[automat->stateEval(DIGIT)]();
+	tansition_action[automat->stateEval(SIGN)]();
+	tansition_action[automat->stateEval(EQUALS)]();
+	tansition_action[automat->stateEval(COLON)]();
+	tansition_action[automat->stateEval(COLON)]();
+	tansition_action[automat->stateEval(EQUALS)]();
+	tansition_action[automat->stateEval(LETTER)]();
+	tansition_action[automat->stateEval(LETTER)]();
+	tansition_action[automat->stateEval(EQUALS)]();
+
+	return 1;
 }
